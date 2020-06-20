55cb1219250e05d03ba3e5857b49942b2da5cf21
DreiPixel <Henrik.eickmeyer@gmail.com>
2020-06-20 21:22:25 +0200

    added shitty exception filter

--- Sorter/Sorter.py
+++ Sorter/Sorter.py
@@ -8,16 +8,20 @@ def pathinput():
     print("Numbered Folder sorter. \n basically sorts a number of files into numbered files. \n example: folder 1 has 4240 files and you specify that every folder only needs 1000 files \n this script automatically creates new folders and sorts them. \n DISCLAIMER: i have no idea how to optimize this since this is my first python script. \n")    
     while True:
         user_input = input("folder that contains numbered folders: ")
+        user_exceptions = input("Any folder i should ignore? (example: .sync, memes, etc : ")            
         if os.path.exists(user_input) == False:
             print("path not found: " + str(user_input))
         else:
+            directory = os.listdir(user_input)            
             try:
-                directory = os.listdir(user_input)
+                directory.remove(user_exceptions)
                 directory.sort(key=lambda line: int(line.split()[0]))
-                return(user_input)
+                return(user_input, user_exceptions)
                 break
-            except: 
-                print("folder contains folder with a string. please use only numbered folders")              
+            except ValueError as e: 
+                print("folder contains folder with a string. please use only numbered folders \n or the exceptions folder doesnt exist." , e )              
+
+user_input, user_exceptions = pathinput()
 
 def filenumber():
     while True:
@@ -28,11 +32,12 @@ def filenumber():
         except ValueError:
             print("not a number")
         
-user_input = pathinput()
+#user_input = pathinput()
 user_files = filenumber()
 
 def sortdirectorylist():    
     directory = os.listdir(user_input)
+    directory.remove(user_exceptions)
     directory.sort(key=lambda line: int(line.split()[0]))    
     return directory
 
@@ -80,5 +85,6 @@ while len(sortthis) != False:
     else:
         sortinto2 = os.path.join(user_input, str(sortinto[0]))
         shutil.move(os.path.join(user_input, str(sortthis[0]), file), sortinto2)
+print("Finished / Nothing to sort.")
 
 
